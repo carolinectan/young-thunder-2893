e955b57f141e60d9282c844c09ccfae34532dca6
Caroline Tan <carolinectan3@gmail.com>
2021-07-19 13:02:05 -0400

    Add comments

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,5 +7,7 @@ class Actor < ApplicationRecord
 
   def self.average_age
     self.average(:age).to_i.round
+    #why does this have to be converted to an integer first
+    #returns as nil unless converted to_i
   end
 end
