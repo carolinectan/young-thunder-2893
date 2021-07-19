46a4d64d5326f3281229018c288b1a7774a363c6
Caroline Tan <carolinectan3@gmail.com>
2021-07-19 13:26:53 -0400

    Add add new actor form test

--- spec/features/actors/create_spec.rb
+++ spec/features/actors/create_spec.rb
@@ -18,4 +18,30 @@ RSpec.describe 'add actors to a movie' do
 
     expect(current_path).to eq("/movies/#{hitch.id}/add_new_actor")
   end
+
+  it 'link to adding a new actor from a movie show page' do
+    warner = Studio.create!(name: 'Warner Bros', location: 'Los Angeles')
+    hitch = warner.movies.create!(title: 'Hitch', creation_year: 2005, genre: 'Romantic Comedy')
+    will = hitch.actors.create!(name: 'Will Smith', age: 45)
+    eva = hitch.actors.create!(name: 'Eva Mendes', age: 41)
+
+    visit "/movies/#{hitch.id}"
+
+    expect(page).to have_content(will.name)
+    expect(page).to have_content(eva.name)
+    expect(page).to_not have_content("Amber")
+
+    click_button('Add New Actor')
+
+    expect(current_path).to eq("/movies/#{hitch.id}/add_new_actor")
+
+    fill_in(:name, with: 'Amber Valletta')
+    fill_in(:age, with: '43')
+    click_button('Add New Actor')
+
+    expect(current_path).to eq("/movies/#{hitch.id}")
+    expect(page).to have_content(will.name)
+    expect(page).to have_content(eva.name)
+    expect(page).to have_content("Amber")
+  end
 end
